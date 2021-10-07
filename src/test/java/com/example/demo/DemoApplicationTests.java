720e9c154d4db9f3cd02722c5c7c4f5a4b862672
Edgar Ramos <edgar.ramos@mozcalti.com>
2021-10-07 13:54:38 -0500

    Cambia el puerto de ejecución de pruebas
    Se hace para resolver el issue #3
    
    Ahora se usa un puerto aleatorio y se configura RestAssured para usarlo.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,25 +1,36 @@
 package com.example.demo;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.get;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
 
     @Test
     void saludo() {
         get("/hola/Juan")
                 .then()
                 .assertThat()
-                .statusCode(HttpStatus.OK.value())
+                .statusCode(is(HttpStatus.OK.value()))
                 .and()
                 .body("respuesta", is(equalTo("Hola Juan")));
-
     }
 
 }
