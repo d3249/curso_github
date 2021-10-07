0b4f5a2ea901387ab38d9d5a6499c14be6ecd3fb
Edgar Ramos <edgar.ramos@mozcalti.com>
2021-10-07 12:46:30 -0500

    Avanza solución de #2
    Se asocia la clase SaludoController a la URL /hola/NOMBRE para que
    cumpla con lo solicitado

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,24 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
-@SpringBootTest
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void saludo() {
+        get("/hola/Juan")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .and()
+                .body("respuesta", is(equalTo("Hola Juan")));
+
+    }
 
 }
