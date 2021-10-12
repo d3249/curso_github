90d23db903ff164c9e66c17ea294c4f891c43e27
Edgar Ramos <edgar.ramos@mozcalti.com>
2021-10-12 15:39:26 -0500

    Corrige el build
    fix #13

--- src/main/java/com/example/demo/web/controller/SaludoController.java
+++ src/main/java/com/example/demo/web/controller/SaludoController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 import static java.lang.String.format;
 
 @RestController
@@ -18,8 +20,20 @@ public class SaludoController {
 
         var nombreFinal = nombre;
 
-        if (nombre == null || nombre.isBlank())
+        if (nombreFinal == null || nombreFinal.isBlank()) {
             nombreFinal = "Mundo";
+        } else {
+
+            var firstLetter = nombre.substring(0, 1);
+            var rest = nombre.substring(1);
+
+            var stringBuilder = new StringBuilder(firstLetter.toUpperCase(Locale.ROOT));
+
+            stringBuilder.append(rest.toLowerCase(Locale.ROOT));
+
+            nombreFinal = stringBuilder.toString();
+
+        }
 
 
         return new ServerResponse<>(format("Hola %s", nombreFinal.trim()));
